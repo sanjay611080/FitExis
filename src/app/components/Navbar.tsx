132c717ca7139baf5453567357c6f7231d034af9
Mradroit611 <sanjayvermaa12121@gmail.com>
2024-08-10 18:00:02 +0530

    Initial commit: Cloned repository

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -57,7 +57,7 @@ function ResponsiveAppBar() {
               textDecoration: 'none',
             }}
           >
-            LOGO
+            LOGOoos
           </Typography>
 
           <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
